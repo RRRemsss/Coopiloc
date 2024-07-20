884c9f97cec0193b8ccacf5b2484303bfd10fb46
Remy <126864072+RRRemsss@users.noreply.github.com>
2024-07-20 21:04:35 +0200

    Tabs dynamic + New property

--- public/assets/js/tabs.js
+++ public/assets/js/tabs.js
@@ -1,12 +1,17 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const navLinks = document.querySelectorAll('#navTabs .nav-link');
-  
-    navLinks.forEach(link => {
-      link.addEventListener('click', function() {
-        // Remove active class from all links
-        navLinks.forEach(nav => nav.classList.remove('active'));
-        // Add active class to the clicked link
-        this.classList.add('active');
+document.addEventListener('DOMContentLoaded', function () {
+  const tabs = document.querySelectorAll('#form-tabs .nav-link');
+  const sections = document.querySelectorAll('.form-section');
+
+  tabs.forEach(tab => {
+      tab.addEventListener('click', function (e) {
+          e.preventDefault();
+          
+          tabs.forEach(t => t.classList.remove('active'));
+          sections.forEach(s => s.classList.add('d-none'));
+
+          this.classList.add('active');
+          const target = document.querySelector(this.getAttribute('href'));
+          target.classList.remove('d-none');
       });
-    });
   });
+});
